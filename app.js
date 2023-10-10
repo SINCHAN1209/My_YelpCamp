26ff84be692dee088572a4363930776a20718405
Sinchan <sinchan12sep@gmail.com>
2023-10-10 13:59:52 +0530

    port update

--- app.js
+++ app.js
@@ -152,6 +152,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error-template', {err});
 })
 
-app.listen(3000, () => {
-    console.log('Opened at Port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Opened at Port ${port}`)
 })
